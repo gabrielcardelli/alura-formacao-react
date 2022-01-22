22454c0538943f87375248544e5f660e31018257
gabriel <gabrielcastilho.cardelli@gmail.com>
2022-01-21 22:43:50 -0300

    Finalizando o módulo 3

---
+++ react-router-navegacao-spa/petshop/src/components/ListaPost.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+
+import {busca} from '../api/api'
+
+const ListaPost = ({url}) => {
+
+  const [posts,setPosts] = useState([])
+
+  useEffect(() => {
+    busca(url,setPosts)
+  
+  },[])
+  
+  return (
+    <section className="posts container">
+      {console.log(posts)}
+      {posts.map(post => 
+        <Link to="#" className={`cartao-post cartao-post--${post.categoria}`}>
+          <article key={post.id}>
+            <h3 className='cartao-post__titulo'>
+              {post.title}
+            </h3>
+            <p className='cartao-post__meta'>{post.metadescription}</p>
+          </article>
+        </Link>
+      )}
+    </section>
+  )
+}
+
+export default ListaPost
